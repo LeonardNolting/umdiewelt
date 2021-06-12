742b582fe578e1505ad70f5d628f48f498307f26
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-06-13 00:00:53 +0200

    inhalt nacheinander einfaden während scrollen

--- js/inhalt/inhalt.ts
+++ js/inhalt/inhalt.ts
@@ -1,7 +1,24 @@
 import quellen from "./quellen";
 import fakten from "./fakten";
 
+const observer = new IntersectionObserver(((eintraege, observer) => {
+	eintraege.filter(eintrag => eintrag.isIntersecting).forEach(eintrag => {
+		eintrag.target.classList.add("gesehen")
+		observer.unobserve(eintrag.target)
+	})
+}), {
+	// rootMargin: "0px 0px -10% 0px"
+
+	/*rootMargin: '0px 0px -10px 0px',
+	threshold: 0.5*/
+
+	rootMargin: '0px 0px -50px 0px',
+	threshold: 0.3
+})
+
 export default () => {
+	document.querySelectorAll("#inhalt .verspaetet").forEach(element => observer.observe(element))
+
 	fakten()
 	quellen()
 }
