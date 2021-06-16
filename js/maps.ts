571c28be11fee6b43d973efbe6b326fb50f06eb7
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-06-16 17:57:00 +0200

    laden von maps ausgelagert

---
+++ js/maps.ts
@@ -0,0 +1,9 @@
+import step from "./step";
+import {Loader} from "@googlemaps/js-api-loader";
+import {apiSchluessel as apiKey, region, sprache as language, version} from "./konfiguration";
+
+export default () => {
+	step("Lädt Google Maps")
+
+	return new Loader({apiKey, version, language, region}).load()
+}
