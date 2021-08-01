4a6fb9247b1c68a9bbfe19906569ae9299144e47
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-01 21:55:34 +0200

    Benachrichtigungen Übersetzungen & Position

--- js/benachrichtigungen/benachrichtigungen.ts
+++ js/benachrichtigungen/benachrichtigungen.ts
@@ -2,7 +2,20 @@
 import AWN from "awesome-notifications"
 
 const benachrichtigungen = new AWN({
-
+	position: "bottom-left",
+	maxNotifications: 4,
+	labels: {
+		tip: "Tipp",
+		info: "Info",
+		warning: "Achtung",
+		success: "Erfolg",
+		alert: "Fehler",
+		async: "Lädt...",
+	},
+	messages: {
+		async: "Bitte warten Sie einen Moment",
+		"async-block": "Lädt..."
+	}
 })
 
 export default benachrichtigungen
