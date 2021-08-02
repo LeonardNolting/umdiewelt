157388b2f9d6fa7096b8a798d13506845a055255
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-02 22:00:17 +0200

    Warnhinweis für veraltete Browser gestyled

--- js/browserAktualisierung.ts
+++ js/browserAktualisierung.ts
@@ -2,5 +2,7 @@
 import browserUpdate from 'browser-update/update.npm.js';
 
 export default () => browserUpdate({
-	l: "de"
+	l: "de",
+	container: document.getElementById("browser-aktualisierung"),
+	shift_page_down: false
 })
