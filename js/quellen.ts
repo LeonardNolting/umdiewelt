7ce64a674cd9809a62c7d82698d24cd8abf93617
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-10 18:43:53 +0200

    Hinzufügen von möglichen Wortumbrüchen

--- js/quellen.ts
+++ js/quellen.ts
@@ -1,14 +1,14 @@
 const quellen: { [bezeichnung: string]: { was: string, wert: number, name: string, url: string } } = {
 	"verbrauch": {
-		was: "Durchschnittlicher Kraftstoffverbrauch PKW/Kombi mit Otto-Motor in Deutschland 2019: <b>7,8l/100km</b>",
+		was: "Durch&shy;schnitt&shy;licher Kraft&shy;stoff&shy;ver&shy;brauch PKW/Kombi mit Otto-Motor in Deutsch&shy;land 2019: <b>7,8l/100km</b>",
 		wert: 7.8,
-		name: "Verkehr in Zahlen 2020/2021, S. 309, Bundesministerium für Verkehr und digitale Infrastruktur",
+		name: "Verkehr in Zahlen 2020/2021, S. 309, Bundes&shy;ministerium für Verkehr und digitale Infra&shy;struktur",
 		url: "https://www.bmvi.de/SharedDocs/DE/Publikationen/G/verkehr-in-zahlen-2020-pdf.pdf?__blob=publicationFile"
 	},
 	"gespart": {
-		was: "Eingespartes CO<sub>2</sub> pro Kilometer, zurückgelegt mit Fahrrad statt Auto mit Otto-Motor, bei einem Verbrauch von 7,8l/100km: <b>0,18kg/km</b>",
+		was: "Ein&shy;ge&shy;spartes CO<sub>2</sub> pro Kilometer, zurück&shy;gelegt mit Fahr&shy;rad statt Auto mit Otto-Motor, bei einem Ver&shy;brauch von 7,8l/100km: <b>0,18kg/km</b>",
 		wert: 0.18,
-		name: "Einspar Rechner, Allgemeiner Deutscher Fahrrad-Club Landesverband Baden-Württemberg e. V.",
+		name: "Einspar-Rechner, Allge&shy;meiner Deutscher Fahrrad-Club Landes&shy;verband Baden-Würt&shy;tem&shy;berg e.&nbsp;V.",
 		url: "https://www.adfc-bw.de/radzurarbeit/einspar-rechner/"
 	}
 }
