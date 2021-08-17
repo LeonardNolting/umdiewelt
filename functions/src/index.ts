4ff9aacf2e6395e71f4c561c19b234937c65773d
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-17 23:55:25 +0200

    cloud functions zum anpassen der fakten bei hinzugefügten/entfernten strecken

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,30 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+const datenbank = admin.database()
+
+async function passeFaktenAn(data: { strecke: number, fahrer: string }, negativ: boolean = false) {
+	const delta = data.strecke * (negativ ? -1 : 1)
+	const fahrer = data.fahrer
+	const streckeIncrement = admin.database.ServerValue.increment(delta)
+	const anzahlStreckenIncrement = admin.database.ServerValue.increment(1)
+	const laufend = (await datenbank.ref("allgemein/saisons/laufend").get()).val()
+	const {schule, klasse} = (await datenbank.ref("spezifisch/fahrer/" + fahrer).get()).val()
+	const updates: { [ref: string]: any } = {}
+	updates["spezifisch/fahrer/" + fahrer + "/strecke"] = streckeIncrement
+	updates["spezifisch/fahrer/" + fahrer + "/anzahlStrecken"] = anzahlStreckenIncrement
+	updates["spezifisch/klassen/details/" + schule + "/" + klasse + "/strecke"] = streckeIncrement
+	updates["spezifisch/klassen/details/" + schule + "/" + klasse + "/anzahlStrecken"] = anzahlStreckenIncrement
+	updates["allgemein/saisons/details/" + laufend + "/strecke"] = streckeIncrement
+	updates["allgemein/saisons/details/" + laufend + "/anzahlStrecken"] = anzahlStreckenIncrement
+	updates["allgemein/saisons/details/" + laufend + "/schulen/details/" + schule + "/strecke"] = streckeIncrement
+	updates["allgemein/saisons/details/" + laufend + "/schulen/details/" + schule + "/anzahlStrecken"] = anzahlStreckenIncrement
+	updates["allgemein/strecke"] = streckeIncrement
+	updates["allgemein/anzahlStrecken"] = anzahlStreckenIncrement
+	await datenbank.ref().update(updates)
+}
+
+const passeFaktenAnRef = functions.region("europe-west1").database.ref("/spezifisch/strecken/{strecke}")
+export const erhoeheFakten = passeFaktenAnRef.onCreate(snap => passeFaktenAn(snap.val()))
+export const verringereFakten = passeFaktenAnRef.onDelete(snap => passeFaktenAn(snap.val(), true))
