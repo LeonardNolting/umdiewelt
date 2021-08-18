da55ef68b6388d97d4ab7b831065f271d7af2ead
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-18 16:53:18 +0200

    fix scheduleTime cloud tasks nicht von "jetzt" an sondern unix

--- functions/src/saison/utils.ts
+++ functions/src/saison/utils.ts
@@ -3,4 +3,4 @@ import {region} from "../init";
 export const project = JSON.parse(process.env.FIREBASE_CONFIG!).projectId
 export const queueName = 'countdowns'
 export const url = (functionName: string) => `https://${region}-${project}.cloudfunctions.net/${functionName}`
-export const seconds = (datum: number) => (datum - Date.now()) / 1000
+export const seconds = (datum: number) => datum / 1000
