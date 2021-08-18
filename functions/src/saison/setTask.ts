3617311acd5c9f06b63e076a76a112a573d8ab62
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-18 21:36:49 +0200

    countdown verwaltung client reusen und import mit side effects? nutzen

--- functions/src/saison/setTask.ts
+++ functions/src/saison/setTask.ts
@@ -1,14 +1,15 @@
 import {region} from "../init";
-import {CloudTasksClient} from "@google-cloud/tasks";
 import {Change} from "firebase-functions";
 import {DataSnapshot} from "firebase-functions/lib/providers/database";
 
+const {CloudTasksClient} = require("@google-cloud/tasks")
+
+const tasksClient = new CloudTasksClient()
 const project = JSON.parse(process.env.FIREBASE_CONFIG!).projectId
 const queueName = 'countdowns'
 const url = (functionName: string) => `https://${region}-${project}.cloudfunctions.net/${functionName}`
 const seconds = (datum: number) => datum / 1000
 export const setTask = async ({before, after}: Change<DataSnapshot>, functionName: string) => {
-	const tasksClient = new CloudTasksClient()
 	const queuePath = tasksClient.queuePath(project, region, queueName)
 
 	// Bestehenden Task entfernen
