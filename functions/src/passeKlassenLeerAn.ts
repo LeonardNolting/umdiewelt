37ea5b36cd181bf6d8b8bc4c39eaa324bec7a968
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-19 19:24:00 +0200

    passeKlassenLeerAn falschen Namen gefixed

--- functions/src/passeKlassenLeerAn.ts
+++ functions/src/passeKlassenLeerAn.ts
@@ -1,5 +1,5 @@
 import {datenbank, region} from "./init";
 import * as functions from "firebase-functions";
 
-export const passeKlassenLeerAnRef = functions.region(region).database.ref("/spezifisch/klassen/liste")
+export const passeKlassenLeerAn = functions.region(region).database.ref("/spezifisch/klassen/liste")
 	.onWrite(({after}) => datenbank.ref("/spezifisch/klassen/leer").set(after.numChildren() === 0))
