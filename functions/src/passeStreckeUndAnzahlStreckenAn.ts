d6551bbba0ef8d8642b4a407d99ea32a3e33b603
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-20 22:49:05 +0200

    passeStreckeUndAnzahlStreckenAn nur wenn bei laufender Saison

--- functions/src/passeStreckeUndAnzahlStreckenAn.ts
+++ functions/src/passeStreckeUndAnzahlStreckenAn.ts
@@ -3,12 +3,16 @@ import * as functions from "firebase-functions";
 import {datenbank, region} from "./init";
 
 async function passeStreckeUndAnzahlStreckenAn(data: { strecke: number, fahrer: string }, negativ: boolean = false) {
+	const laufend = (await datenbank.ref("allgemein/saisons/laufend").get()).val()
+
+	// Eintragen neuer Saison löscht allerhand Strecken - aber soll nicht Statistiken wieder runterzählen
+	if (laufend === null) return
+
 	const delta = data.strecke
 	const fahrer = data.fahrer
 	const richtung = negativ ? -1 : 1
 	const streckeIncrement = admin.database.ServerValue.increment(delta * richtung)
 	const anzahlStreckenIncrement = admin.database.ServerValue.increment(1 * richtung)
-	const laufend = (await datenbank.ref("allgemein/saisons/laufend").get()).val()
 	const {schule, klasse} = (await datenbank.ref("spezifisch/fahrer/" + fahrer).get()).val()
 	const updates: { [ref: string]: any } = {}
 	updates["spezifisch/fahrer/" + fahrer + "/strecke"] = streckeIncrement
