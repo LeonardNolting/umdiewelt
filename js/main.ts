9bfbecda4d0b17b5d0ad309580c8f28620c83eed
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-24 19:43:28 +0200

    wait gefixed

--- js/main.ts
+++ js/main.ts
@@ -11,8 +11,10 @@ import '@fortawesome/fontawesome-free/css/solid.min.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import 'awesome-notifications/dist/style.css'
 import {Authentifizierung} from "./firebase/authentifizierung/authentifizierung";
+import wait from "./wait";
 
 document.addEventListener("DOMContentLoaded", async () => {
+	wait()
 	browserAktualisierung()
 	popups()
 	await Cookies.ueberpruefen()
--- js/wait.ts
+++ js/wait.ts
@@ -1,39 +1,43 @@
-interface Document {
-	wait: boolean
-	progress: boolean
+declare global {
+	interface Document {
+		wait: boolean
+		progress: boolean
+	}
+
+	interface HTMLElement {
+		/**
+		 * Legt im CSS fest, dass dieses Element und alle Kindelemente erreichbar sind, selbst wenn der Rest nicht klickbar ist.
+		 */
+		waitImmune: boolean
+	}
 }
 
-Object.defineProperties(HTMLBodyElement.prototype, {
-	wait: {
-		get() {
-			return this.hasAttribute('data-wait');
+export default () => {
+	Object.defineProperties(Document.prototype, {
+		wait: {
+			get() {
+				return this.documentElement.hasAttribute('data-wait');
+			},
+			set(value: boolean) {
+				(value ? this.documentElement.setAttribute('data-wait', '') : this.documentElement.removeAttribute('data-wait'));
+			},
 		},
-		set(value: boolean) {
-			(value ? this.setAttribute('data-wait', '') : this.removeAttribute('data-wait'));
+		progress: {
+			get() {
+				return this.documentElement.hasAttribute('data-progress');
+			},
+			set(value: boolean) {
+				(value ? this.documentElement.setAttribute('data-progress', '') : this.documentElement.removeAttribute('data-progress'));
+			},
 		},
-	},
-	progress: {
-		get() {
-			return this.hasAttribute('data-progress');
-		},
-		set(value: boolean) {
-			(value ? this.setAttribute('data-progress', '') : this.removeAttribute('data-progress'));
-		},
-	},
-});
+	});
 
-interface HTMLElement {
-	/**
-	 * Legt im CSS fest, dass dieses Element und alle Kindelemente erreichbar sind, selbst wenn der Rest nicht klickbar ist.
-	 */
-	waitImmune: boolean
+	Object.defineProperty(HTMLElement.prototype, 'waitImmune', {
+		get() {
+			return this.hasAttribute('data-wait-immune');
+		},
+		set(value: boolean) {
+			(value ? this.setAttribute('data-wait-immune', '') : this.removeAttribute('data-wait-immune'));
+		},
+	});
 }
-
-Object.defineProperty(Element.prototype, 'waitImmune', {
-	get() {
-		return this.hasAttribute('data-wait-immune');
-	},
-	set(value: boolean) {
-		(value ? this.setAttribute('data-wait-immune', '') : this.removeAttribute('data-wait-immune'));
-	},
-});
