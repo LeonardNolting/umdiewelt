13340fe483f70bb569daebf28cbf46d01499ea8e
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-08-24 19:43:02 +0200

    load neutral gegenüber promise.reject

--- js/load.ts
+++ js/load.ts
@@ -1,11 +1,10 @@
-// import HTMLElement from "./wait"
-
 const toLoad: Promise<any>[] = []
 
 export default async function (promise: Promise<void>) {
 	document.wait = true
 	toLoad.push(promise)
-	await promise
-	toLoad.splice(toLoad.indexOf(promise), 1)
-	if (toLoad.length === 0) document.wait = false
+	promise.finally(() => {
+		toLoad.splice(toLoad.indexOf(promise), 1)
+		if (toLoad.length === 0) document.wait = false
+	})
 }
