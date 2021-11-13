0e4862dec055a6f140abd0c91cbf49dd1466596c
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-11-13 15:40:11 +0100

    fix: Quellen in popoup auslagern

--- js/inhalt/quellen.ts
+++ js/inhalt/quellen.ts
@@ -1,7 +1,14 @@
 import quellen from "../quellen";
+import Popup from "../popup";
 
 const liste = document.getElementById("quellen-anzeige")
+const popup = document.getElementById("popup-quellen")
 export default () => {
+	document.getElementById("popup-quellen-fertig").addEventListener("click", () => Popup.schliessen(popup))
+	document.getElementById("quellen").addEventListener("click",
+		() => Popup.oeffnen(popup)
+	)
+
 	Object.values(quellen).forEach(({name, url, was, wert}) => {
 		const listenElement = document.createElement("li")
 
