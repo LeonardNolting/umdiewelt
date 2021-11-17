9bdf4c80f8522951d87814c293c2e60a3517f886
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-11-17 20:04:39 +0100

    fix: <any> -> as any

--- js/cookies.ts
+++ js/cookies.ts
@@ -9,9 +9,9 @@ namespace Cookies {
 		// "as any" erlaubt index access operator (CookieEinstellung[...])
 		// s. https://stackoverflow.com/questions/62215454/how-to-get-enum-key-by-value-in-typescript
 
-		ALLE = <any>"alle",
-		NOTWENDIG = <any>"notwendig",
-		KEINE = <any>"keine",
+		ALLE = "alle" as any,
+		NOTWENDIG = "notwendig" as any,
+		KEINE = "keine" as any,
 		UNDEFINED = 0
 	}
 
