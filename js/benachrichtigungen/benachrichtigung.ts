82a337391e655c2ead1b6e3bef892ccaf1b09e84
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-11-28 11:15:01 +0100

    perf: CSS für Benachrichtigungen erst laden, wenn Benachrichtigung auftaucht

--- js/benachrichtigungen/benachrichtigung.ts
+++ js/benachrichtigungen/benachrichtigung.ts
@@ -2,10 +2,17 @@ import benachrichtigungen from "./benachrichtigungen";
 import BenachrichtigungsLevel from "./benachrichtigungsLevel";
 import BenachrichtigungsOptionen from "./benachrichtigungsOptionen";
 
+let init = false
 export default function benachrichtigung(
 	nachricht: string,
 	level: BenachrichtigungsLevel = BenachrichtigungsLevel.INFO,
 	optionen: BenachrichtigungsOptionen = {}
 ) {
+	if (!init) {
+		init = true
+		import('@fortawesome/fontawesome-free/css/solid.min.css')
+		import('@fortawesome/fontawesome-free/css/fontawesome.min.css')
+		import('awesome-notifications/dist/style.css')
+	}
 	return benachrichtigungen[level](nachricht, optionen)
 }
--- js/main.ts
+++ js/main.ts
@@ -4,9 +4,6 @@ import Cookies from "./cookies";
 import popups from "./popups";
 import browserAktualisierung from "./browserAktualisierung";
 
-import '@fortawesome/fontawesome-free/css/solid.min.css'
-import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
-import 'awesome-notifications/dist/style.css'
 import wait from "./wait";
 import load from "./load";
 import {Eintragung} from "./eintragen";
