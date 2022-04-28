34930d3c8dba7ca60e0ec99fc1df7bdcf8b7d5f3
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2022-04-28 21:15:30 +0200

    fix: auf Richtigkeit der Strecken-ID achten bei strecke löschen (admin)

--- js/admin/kontrollen/strecke-loeschen.ts
+++ js/admin/kontrollen/strecke-loeschen.ts
@@ -1,7 +1,8 @@
 import Kontrolle from "./kontrolle";
-import {ref, remove} from "firebase/database";
+import {get, ref, remove} from "firebase/database";
 import Datenbank from "../../firebase/datenbank";
 import benachrichtigung from "../../benachrichtigungen/benachrichtigung";
+import BenachrichtigungsLevel from "../../benachrichtigungen/benachrichtigungsLevel";
 
 export default class StreckeLoeschenKontrolle extends Kontrolle {
 	constructor() {
@@ -16,9 +17,13 @@ export default class StreckeLoeschenKontrolle extends Kontrolle {
 	}
 
 	protected async submit(): Promise<string> {
-		const id = this.element("id").value
+		const id = (this.element("id") as HTMLInputElement).value.trim()
 		if (!id || id.length <= 0) return benachrichtigung("Bitte eine gültige ID eingeben.")
-		return await remove(ref(Datenbank.datenbank, "spezifisch/strecken/" + id)).then(() => "Strecke gelöscht.")
+
+		const streckeRef = ref(Datenbank.datenbank, "spezifisch/strecken/" + id);
+		const strecke = await get(streckeRef)
+		if (!strecke.exists()) return benachrichtigung("Die angegebene Strecke konnte nicht gefunden werden. Bitte leiten Sie die E-Mail-Meldung an leonolting@gmail.com weiter.", BenachrichtigungsLevel.ALARM)
+		return await remove(streckeRef).then(() => "Strecke gelöscht.")
 	}
 
 	protected async vorbereiten(): Promise<void> {
