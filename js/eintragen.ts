7a7a9e1e2169541d08a81b2ee3525dfcbf767b8c
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2022-05-03 17:08:06 +0200

    fix: wartung signalisieren

--- js/eintragen.ts
+++ js/eintragen.ts
@@ -285,6 +285,7 @@ export class Eintragung {
 	static berechnenAnkunft = document.getElementById("eintragen-suche-b") as HTMLInputElement
 
 	async oeffnen() {
+		return benachrichtigung("Es können aktuell keine Eintragungen vorgenommen werden. Probiere es bitte in einer Stunde nochmal.")
 		// return benachrichtigung("Es können aktuell keine Eintragungen vorgenommen werden.")
 
 		// Abbrechen falls schon eine Eintragung offen
