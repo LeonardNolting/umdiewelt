88a95cfee2452b3e4de3349d100a6ae6f5a8e38b
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2022-05-06 17:13:35 +0200

    fix: fahrer löschen wenn alle strecken gelöscht wurden

--- js/admin/kontrollen/strecke-loeschen.ts
+++ js/admin/kontrollen/strecke-loeschen.ts
@@ -1,5 +1,5 @@
 import Kontrolle from "./kontrolle";
-import {get, ref, remove} from "firebase/database";
+import {equalTo, get, orderByChild, query, ref, remove} from "firebase/database";
 import Datenbank from "../../firebase/datenbank";
 import benachrichtigung from "../../benachrichtigungen/benachrichtigung";
 import BenachrichtigungsLevel from "../../benachrichtigungen/benachrichtigungsLevel";
@@ -23,6 +23,15 @@ export default class StreckeLoeschenKontrolle extends Kontrolle {
 		const streckeRef = ref(Datenbank.datenbank, "spezifisch/strecken/" + id);
 		const strecke = await get(streckeRef)
 		if (!strecke.exists()) return benachrichtigung("Die angegebene Strecke konnte nicht gefunden werden. Bitte leiten Sie die E-Mail-Meldung an leonolting@gmail.com weiter.", BenachrichtigungsLevel.ALARM)
+
+		const fahrerId = strecke["fahrer"];
+		if (!fahrerId || fahrerId) return benachrichtigung("Konnte Fahrer der Strecke nicht identifizieren.", BenachrichtigungsLevel.ALARM)
+		const fahrerRef = ref(Datenbank.datenbank, "spezifisch/fahrer/" + fahrerId);
+
+		// Wenn keine Strecken bei diesem Fahrer verbleiben, Fahrer auch löschen
+		const streckenIds = await get(query(ref(Datenbank.datenbank, "spezifisch/strecken/"), orderByChild("fahrer"), equalTo(id))).then(result => Object.keys(result.val()))
+		if (streckenIds.length === 0) await remove(fahrerRef)
+
 		return await remove(streckeRef).then(() => "Strecke gelöscht.")
 	}
 
