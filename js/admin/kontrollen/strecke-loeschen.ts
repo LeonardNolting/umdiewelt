ea85130a8b00ae0051e455be95ed41e54bc9b77e
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2022-05-12 22:16:39 +0200

    fix: fahrer löschen wenn alle strecken gelöscht wurden bugs fixen

--- js/admin/kontrollen/strecke-loeschen.ts
+++ js/admin/kontrollen/strecke-loeschen.ts
@@ -24,12 +24,12 @@ export default class StreckeLoeschenKontrolle extends Kontrolle {
 		const strecke = await get(streckeRef)
 		if (!strecke.exists()) return benachrichtigung("Die angegebene Strecke konnte nicht gefunden werden. Bitte leiten Sie die E-Mail-Meldung an leonolting@gmail.com weiter.", BenachrichtigungsLevel.ALARM)
 
-		const fahrerId = strecke["fahrer"];
+		const fahrerId = strecke.val()["fahrer"];
 		if (!fahrerId || fahrerId.length < 1) return benachrichtigung("Konnte Fahrer der Strecke nicht identifizieren.", BenachrichtigungsLevel.ALARM)
 		const fahrerRef = ref(Datenbank.datenbank, "spezifisch/fahrer/" + fahrerId);
 
 		// Wenn keine Strecken bei diesem Fahrer verbleiben, Fahrer auch löschen
-		const streckenIds = await get(query(ref(Datenbank.datenbank, "spezifisch/strecken/"), orderByChild("fahrer"), equalTo(id))).then(result => Object.keys(result.val()))
+		const streckenIds = await get(query(ref(Datenbank.datenbank, "spezifisch/strecken/"), orderByChild("fahrer"), equalTo(id))).then(result => Object.keys(result.val() || {}))
 		if (streckenIds.length === 0) await remove(fahrerRef)
 
 		return await remove(streckeRef).then(() => "Strecke gelöscht.")
