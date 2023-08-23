47872a14cbca13370ef33197338350a3813ac96c
Leonard Nolting <leonard.nolting@tum.de>
2023-08-23 23:22:55 +0200

    feat: Nachricht über doppelte Umrundung anzeigen

--- js/inhalt/erfolg.ts
+++ js/inhalt/erfolg.ts
@@ -1,5 +1,20 @@
 import {umfang} from "../konfiguration";
 
 export default (strecke: number) => {
-	document.getElementById("nachricht").classList[strecke >= umfang ? "add" : "remove"]("sichtbar")
+	const nachricht1 = document.getElementById("nachricht");
+	const nachricht2 = document.getElementById("nachricht");
+
+	const show = (el: HTMLElement) => el.classList.add("sichtbar"),
+		hide = (el: HTMLElement) => el.classList.remove("sichtbar")
+
+	if (strecke < umfang) {
+		hide(nachricht1)
+		hide(nachricht2)
+	} else if (strecke < 2 * umfang) {
+		show(nachricht1)
+		hide(nachricht2)
+	} else {
+		hide(nachricht1)
+		show(nachricht2)
+	}
 }
