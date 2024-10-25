b17cffe24dd169b7169002cc10df76346f7f9385
Leonard Nolting <leonolting@gmail.com>
2024-10-25 03:27:23 +0200

    Nachricht zu 3 Umrundungen anzeigen

--- js/inhalt/erfolg.ts
+++ js/inhalt/erfolg.ts
@@ -3,6 +3,7 @@ import {umfang} from "../konfiguration";
 export default (strecke: number) => {
 	const nachricht1 = document.getElementById("nachricht");
 	const nachricht2 = document.getElementById("nachricht3");
+	const nachricht3 = document.getElementById("nachricht5");
 
 	const show = (el: HTMLElement) => el.classList.add("sichtbar"),
 		hide = (el: HTMLElement) => el.classList.remove("sichtbar")
@@ -10,11 +11,18 @@ export default (strecke: number) => {
 	if (strecke < umfang) {
 		hide(nachricht1)
 		hide(nachricht2)
+		hide(nachricht3)
 	} else if (strecke < 2 * umfang) {
 		show(nachricht1)
 		hide(nachricht2)
-	} else {
+		hide(nachricht3)
+	} else if (strecke < 3 * umfang) {
 		hide(nachricht1)
 		show(nachricht2)
+		hide(nachricht3)
+	} else {
+		hide(nachricht1)
+		hide(nachricht2)
+		show(nachricht3)
 	}
 }
